e82c83af313bcbbf58526593a89233c756ef8c29
Prashant <octanesingh@gmail.com>
2022-04-09 16:06:11 +0530

    cron'

--- pages/api/sendmail.js
+++ pages/api/sendmail.js
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import sendMail from "./functions/sendMail"
 export default async function handler(req, res) {
+  setInterval(()=>{console.log("Tiger zinda hai")},30000)
   await sendMail('octanesingh@gmail.com','Subject',"Hey you there");
   res.status(200).json({ name: 'John Doe' })
 }
