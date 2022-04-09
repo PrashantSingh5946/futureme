b428de797dbb92b3a783cd771c6f1465cfcc60c8
Prashant <octanesingh@gmail.com>
2022-04-09 16:33:47 +0530

    Resetting the async method

--- pages/api/sendmail.js
+++ pages/api/sendmail.js
@@ -1,8 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import sendMail from "./functions/sendMail"
 export default async function handler(req, res) {
-  setInterval(async () => {
-    await sendMail('octanesingh@gmail.com', 'Subject', "Hey you there");
+  setInterval(() => {
+    sendMail('octanesingh@gmail.com', 'Subject', "Hey you there");
     console.log("Tiger zinda hai")
   }, 30000)
   
