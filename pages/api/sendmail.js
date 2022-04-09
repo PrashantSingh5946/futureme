c4196c8001acffeaf96dff4de13291292bf17a79
Prashant <octanesingh@gmail.com>
2022-04-09 16:16:35 +0530

    Async arrow functions

--- pages/api/sendmail.js
+++ pages/api/sendmail.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import sendMail from "./functions/sendMail"
 export default async function handler(req, res) {
-  setInterval(() => {
+  setInterval(async () => {
     await sendMail('octanesingh@gmail.com', 'Subject', "Hey you there");
     console.log("Tiger zinda hai")
   }, 300000)
