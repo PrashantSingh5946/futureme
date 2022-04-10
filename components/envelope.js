cf42e4468de719a9b9c170fa531442511b5747a4
Prashant <octanesingh@gmail.com>
2022-04-10 20:51:17 +0530

    Envelope created successfully

---
+++ components/envelope.js
@@ -0,0 +1,10 @@
+import styles from './envelope.module.css'
+export default function Envelope() {
+  return (
+    <div className={styles.envelope}>
+<div className={styles.paper}>
+  
+</div>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Envelope from '../components/envelope'
 
 export default function Home() {
   return (
-    
-
       <div className={styles.background}>
         <span></span>
         <span></span>
@@ -27,9 +26,7 @@ export default function Home() {
         <span></span>
         <span></span>
         <span></span>
+        <Envelope/>
       </div> 
-    
-    
-   
   )
 }
