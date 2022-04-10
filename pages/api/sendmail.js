0d7d59c7756043056b7fe147b923035d5e8332dd
Prashant <octanesingh@gmail.com>
2022-04-10 14:07:28 +0530

    Restructuring the scheduler

--- pages/api/sendmail.js
+++ pages/api/sendmail.js
@@ -1,10 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import sendMail from "./functions/sendMail"
 export default async function handler(req, res) {
-  setInterval(() => {
-    sendMail('octanesingh@gmail.com', 'Subject', "Hey you there");
-    console.log("Tiger zinda hai")
-  }, 30000)
-  
+
+  await sendMail('octanesingh@gmail.com', 'Subject', "Hey you there");
+  console.log("Tiger zinda hai")
+
+
   res.status(200).json({ name: 'John Doe' })
+
 }
